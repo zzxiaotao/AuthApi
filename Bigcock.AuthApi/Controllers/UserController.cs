9b1c4a39f53eef8903d620be8b9d3a7387c192ba
bgg <1625897646@qq.com>
2018-09-21 01:25:13 +0800

    fix bug

---
+++ Bigcock.AuthApi/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bigcock.AuthApiCore;
+using Bigcock.Data.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bigcock.AuthApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        
+    }
+}
